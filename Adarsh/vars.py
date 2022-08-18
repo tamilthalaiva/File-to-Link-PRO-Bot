df0ba90ccf070da6036b21b240e54498cb5f327f
amit4405 <83196950+amit4405@users.noreply.github.com>
2022-08-18 09:52:41 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -19,10 +19,10 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5505368975").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5076949930").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "aaotts"))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "ajak4405"))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
